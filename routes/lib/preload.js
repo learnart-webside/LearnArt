0c190f897fd6906a255c7614477f14a13e9d4cc7
Manojkumar B <manojrec138@gmail.com>
2019-03-24 07:44:22 +0530

    Delete preload.js

--- routes/lib/preload.js
+++
@@ -1,7 +0,0 @@
-function preload() {
-  
-  finds_data = loadBytes('/data/Module1/1line.bin');
-  monalisa_data = loadBytes('/data/Module1/TheMonaLisa.bin');
-  bulldozer_data = loadBytes('/data/Module1/bulldozer.bin');
-  
-}
